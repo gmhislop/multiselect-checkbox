2064aca11be21210bbf65dd6fcad4b3007e45c9b
Giovanni Hislop <giovanni.hislop@dytter.com>
2022-12-06 22:39:40 +0100

    fix. js files

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const selectBtn = document.querySelector(".select-btn"),
+      items = document.querySelectorAll(".item");
+
 selectBtn.addEventListener("click", () => {
     selectBtn.classList.toggle("open");
 });
